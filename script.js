bdca2c9b8f0a55961083ac2a7d3e36a81aea228b
Ivan Higa <ivzeq.higa@gmail.com>
2022-01-01 16:10:38 -0300

    Initial UI

--- script.js
+++ script.js
@@ -14,14 +14,19 @@ function divide(dividend,divisor){
     return dividend/divisor;
 }
 
+function power(base,exponent){
+    return base**exponent;
+}
+
 function operate(operator,number1,number2){
     return(
         operator == '+'? add(parseInt(number1),parseInt(number2)):
         operator == '-'? substract(parseInt(number1),parseInt(number2)): 
         operator == '*'? multiply(parseInt(number1),parseInt(number2)):
         operator == '/'? divide(parseInt(number1),parseInt(number2)):
+        operator == '^'? power(parseInt(number1),parseInt(number2)):
         null
     );
 }
 
-console.log(operate(prompt(),prompt(),prompt()));
+console.log(operate('^',6,2));
